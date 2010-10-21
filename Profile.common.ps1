69a776526861b266cac90485c4b4a88bcc3720d1
staxmanade <jason@elegantcode.com>
2010-10-21 11:03:53 -0700

    added a Load-Assembly helper function

--- Profile.common.ps1
+++ Profile.common.ps1
@@ -6,6 +6,22 @@ function loadCurrentFolderIntoExplorer
 }
 set-alias e loadCurrentFolderIntoExplorer
 
+function Load-Assembly($assembly)
+{
+echo $assembly
+	if(test-path $assembly)
+	{
+		$assemblyPath = get-item $assembly
+		[System.Reflection.Assembly]::LoadFrom($assemblyPath)
+	}
+	else
+	{
+		[System.Reflection.Assembly]::LoadWithPartialName("$assembly")
+	}
+}
+
+
+
 function invoke-psake-locally
 {
 	# Helper script for those who want to run psake without importing the module.
