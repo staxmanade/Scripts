435b062ba2522cd16c5746b1fee5411139851588
Jason Jarrett <jason@elegantcode.com>
2010-11-07 22:57:21 -0800

    re-worked the splitting and made it work with a simple command loop

--- tfs-parse.ps1
+++ tfs-parse.ps1
@@ -4,74 +4,50 @@ function startsWithIgnoreCase([string] $value, [string] $startsWith)
 	$value.StartsWith($startsWith, [System.StringComparison]::OrdinalIgnoreCase)
 }
 
-function chomp($items)
-{
-	if(!$items)
-	{
-		echo "`$items is null"
-		return;
-	}
-	if($items.length -eq 0)
-	{
-		echo "`$items is empty)"
-		return;
-	}
-	
-	$length = $items.length-1
-	if($length -eq 2)
-	{
-		$mid = 1;
-	}
-	else
-	{
-		$mid = [int]($length / 2)
-	}
-
-	echo "items:$items"
-	echo "mid item:$($items[$mid])"
-	echo "mid:$mid"
-	echo "length:$length"
-	
-	$itemMessage = "$($items[$mid])"
-	$doesThisHaveBug = read-host -prompt "Does [$itemMessage] have the bug?(Y/N/Q(quit))"
-
-	if( $doesThisHaveBug -and (startsWithIgnoreCase $doesThisHaveBug 'q'))
-	{
-		write-host "done...(TODO: reset to TFS get-latest...)"
-		return;
-	}
-	
-	
-	if( $doesThisHaveBug -and (startsWithIgnoreCase $doesThisHaveBug 'n'))
-	{
-		$newItems = $items | select -last $mid
-		chomp($newItems)
-	}
-	elseif($doesThisHaveBug -and (startsWithIgnoreCase $doesThisHaveBug 'y'))
-	{
-		$newItems = $items | select -first $mid
-		chomp($newItems)
-	}
-	
-}
-
 function Assert-Arrays-Equal($actual, $expected)
 {
-	if(!$actual)
-	{
-		throw "Assert-Arrays-Equal - actual is null"
-	}
-	if(!$expected)
-	{
-		throw "Assert-Arrays-Equal - expected is null"
-	}
-	$r = Compare-Object $actual $expected
-	if($r)
+	if(!$actual -and $expected)
 	{
 		"actual  : $actual"
 		"expected: $expected"
-		$r
-		throw "Arrays not equal"
+		throw "Assert-Arrays-Equal - actual is null"
+	}
+	if(!$expected -and $actual)
+	{
+		"actual  : $actual"
+		"expected: $expected"
+		throw "Assert-Arrays-Equal - expected is null"
+	}
+	if(!$expected -and !$actual)
+	{
+	}
+	else
+	{
+		$r = Compare-Object $actual $expected
+		if($r)
+		{
+			"actual  : $actual"
+			"expected: $expected"
+			$r
+			throw "Arrays not equal"
+		}
+	}
+}
+
+function Assert-Equal($actual, $expected, $message ='')
+{
+	if(!($actual -eq $expected))
+	{
+		"actual  : $actual"
+		"expected: $expected"
+		if($message)
+		{
+			throw $message
+		}
+		else
+		{
+			throw "expect not equal actual"
+		}
 	}
 }
 
@@ -89,32 +65,97 @@ function Assert
   if (!$conditionToCheck) { throw $failureMessage }
 }
 
-function chompX($items, $wichHalf)
+function midChomp($items)
 {
-	$length = $items.length
-	$mid = [int]($length / 2)
-	
-	if(($length / 2) -gt $mid)
+	$length = $items.Length-1
+	if($length -eq 1)
 	{
-		$mid = ($mid + 1)
-	}
-
-	if($wichHalf -eq 'first')
-	{
-		$items | select -first $mid
+		$mid = 0
 	}
 	else
 	{
-		$items | select -last $mid
+		$mid = [int]($length / 2)
+		if(($length / 2) -gt $mid)
+		{
+			$mid = ($mid + 1)
+		}
+	}
+	write-host "$items"
+	write-host "$length"
+	write-host "$mid"
+	$result = @{
+		Index = $mid
+		Left = $items | select -first ($mid)
+		Right = $items | select -last ($length - $mid)
+	}
+	$result
+}
+
+$result = midChomp @(1,2)
+Assert-Equal $result.Index 0
+Assert-Arrays-Equal $result.Left @()
+Assert-Arrays-Equal $result.Right @(2)
+
+$result = midChomp @(1,2,3)
+Assert-Equal $result.Index 1
+Assert-Arrays-Equal $result.Left @(1)
+Assert-Arrays-Equal $result.Right @(3)
+
+$result = midChomp @(1,2,3,4,5,6,7,8,9)
+Assert-Equal $result.Index 4
+Assert-Arrays-Equal $result.Left @(1,2,3,4)
+Assert-Arrays-Equal $result.Right @(6,7,8,9)
+
+$result = midChomp @('a','b','c','d','e','f','g','h','i')
+Assert-Equal $result.Index 4
+Assert-Arrays-Equal $result.Left @('a','b','c','d')
+Assert-Arrays-Equal $result.Right @('f','g','h','i')
+
+$result = midChomp @('b')
+Assert-Equal $result.Index 0
+Assert-Arrays-Equal $result.Left @()
+Assert-Arrays-Equal $result.Right @()
+
+function chomp($items)
+{
+	if(!($items))
+	{
+		$items
+		'DONE?'
+		return;
+	}
+	$result = midChomp $items
+	'*********** DEBUG'
+	$result
+	'*********** DEBUG'
+
+	$itemX = $items[$result.Index]
+
+	$doesThisHaveBug = read-host -prompt "Does [$itemX] have the bug?(Y/N/Q(quit))"
+
+	if($doesThisHaveBug)
+	{
+		if((startsWithIgnoreCase $doesThisHaveBug 'q'))
+		{
+			write-host "done...(TODO: reset to TFS get-latest...)"
+			return;
+		}
+		
+		if(startsWithIgnoreCase $doesThisHaveBug 'n')
+		{
+		'right'
+			chomp($result.Right)
+		}
+		elseif(startsWithIgnoreCase $doesThisHaveBug 'y')
+		{
+		'left'
+			chomp($result.Left)
+		}
+		else
+		{
+			write-host "Unknown input [$doesThisHaveBug]";
+		}
 	}
 }
 
-$result = chompX @(1,2,3,4,5,6,7,8,9,10,11,12,13) 'first'; Assert-Arrays-Equal $result @(1,2,3,4,5,6,7)
-$result = chompX @(1,2,3,4,5,6,7,8,9,10,11,12,13) 'last';  Assert-Arrays-Equal $result @(7,8,9,10,11,12,13)
-$result = chompX @(1,2) 'first'; Assert-Arrays-Equal $result @(1)
-$result = chompX @(1,2) 'last'; Assert-Arrays-Equal $result @(2)
-$result = chompX @(1,2,3) 'first'; Assert-Arrays-Equal $result @(1,2)
-$result = chompX @(1,2,3) 'last'; Assert-Arrays-Equal $result @(2,3)
-
-$result = chompX @(1,2,3,4) 'first'; Assert-Arrays-Equal $result @(1,2)
-$result = chompX @(1,2,3,4) 'last'; Assert-Arrays-Equal $result @(3,4)
+chomp @('a','b','c','d','e','f','g','h','i')
