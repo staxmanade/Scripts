35364f64da6dc0a41b87a51d39a2e8a06294dad9
Jason Jarrett <jason@elegantcode.com>
2011-06-25 20:51:49 -0700

    added a git init helper that downloads a default .gitignore file

--- Profile.common.ps1
+++ Profile.common.ps1
@@ -187,6 +187,21 @@ function downloadCommonProfile()
 	[Environment]::SetEnvironmentVariable("LastDownloadOfCommonProfile", (midnight), "User")
 }
 
+function gitinit()
+{
+	git init
+	if($LASTEXITCODE)
+	{
+		return;
+	}
+
+	$gitIgnoreRemotePath = 'https://raw.github.com/staxmanade/Scripts/master/.gitignore.rename'
+	$destinationPath = "$(pwd)\.gitignore"
+	$clnt = new-object System.Net.WebClient
+	echo "Downloading base .gitignore from - $gitIgnoreRemotePath"
+	$clnt.DownloadFile($gitIgnoreRemotePath, "$destinationPath")
+}
+
 $tfPath = Find-Program 'Microsoft Visual Studio 10.0\Common7\IDE\TF.exe'
 function tf(){ & $tfPath $args }
 
