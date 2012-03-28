d6d713c4c11a558bb88ac1d2fed7bfd252be5f47
Jason Jarrett <jason@elegantcode.com>
2012-03-27 20:59:33 -0700

    initial creation of my uber developer windows machine setup script

---
+++ devInstall.ps1
@@ -0,0 +1,48 @@
+# install this script by executing
+#  iex ((new-object net.webclient).DownloadString('https://github.com/staxmanade/Scripts/devInstall.ps1'))
+# 
+
+
+# Install Chocolatey from chocolatey.org
+iex ((new-object net.webclient).DownloadString('http://bit.ly/psChocInstall'))
+
+#Install all my favorite packages.
+cinst -all -source 'http://www.myget.org/F/6a72e3c34526424eacb4a37e8c21f809/'
+
+# setup local powershell profile.
+function initProfile()
+{
+    $profileDir = (split-path $profile)
+    if(! (test-path $profileDir))
+    {
+        mkdir $profileDir
+    }
+
+    pushd $profileDir
+
+        if(test-path PsProfile)
+        {
+            pushd PsProfile
+                try{
+					git pull
+                }
+                catch{
+                    $error
+                }
+            popd
+        }
+        else
+        {
+            git clone git@github.com:staxmanade/PsProfile.git
+        }
+
+        if(!(cat $profile | select-string 'PsProfile\\initProfile.ps1'))
+        {
+            "Adding initProfile to $Profile"
+            ". `"$(split-path $profile)\PsProfile\initProfile.ps1`"" | Out-File $profile -append -encoding ASCII
+        }
+        . $profile
+    popd
+}
+
+initProfile
